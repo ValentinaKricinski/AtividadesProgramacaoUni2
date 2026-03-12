19b868b9d3e71e1ff4e2fb9d65b4a0535f8a4d78
Valentina kricinski kuelkamp <valentina.kuelkamp@gmail.com>
2026-03-12 14:36:50 -0300

    Update Uni2Exe19.java

--- Uni2Exe19.java
+++ Uni2Exe19.java
@@ -3,11 +3,26 @@ import java.util.Scanner;
 public class Uni2Exe19 {
     public static void main(String[] args) {
 
-        int a, b, c, d;
-
         Scanner sc = new Scanner(System.in);
-        System.out.println("insira o valor de b: ");
-        
+
+        double B, C, D, A;
+
+        System.out.println("Digite o valor de B:");
+        B = sc.nextDouble();
+
+        System.out.println("Digite o valor de C:");
+        C = sc.nextDouble();
+
+        System.out.println("Digite o valor de D:");
+        D = sc.nextDouble();
+
+        A = B;
+
+        System.out.println("Valor de A: " + A);
+
+        sc.close();
     }
     
+    
 }
+
